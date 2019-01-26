1ab982ed779d3fedb8796bdac8c44ab3369b00f7
tbrownlow8 <tbrownlow8@gatech.edu>
2019-01-25 22:20:25 -0500

    first go at database

---
+++ Escape The Syllabus/Assets/Scripts/User.cs
@@ -0,0 +1,23 @@
+﻿public class User
+{
+    public string username;
+    public int levelsCompleted;
+    public int currentLevel;
+    public int deaths;
+    public int correctAnswers;
+    public int incorrectAnswers;
+
+    public User()
+    {
+    }
+
+    public User(string username)
+    {
+        this.username = username;
+        this.levelsCompleted = 0;
+        this.currentLevel = 1;
+        this.deaths = 0;
+        this.correctAnswers = 0;
+        this.incorrectAnswers = 0;
+    }
+}
