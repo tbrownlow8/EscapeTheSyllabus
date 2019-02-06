b9b6faeb1d9b8a82cdba0d50ec3f9f0c5e4aaeb6
angelseay <angelseay99@gmail.com>
2019-02-06 12:41:36 -0500

    unity game template

---
+++ Escape The Syllabus/Assets/Scripts/CanvasUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasUtil : MonoBehaviour
+{
+  public void EnableCanvas() {
+    this.GetComponent<Canvas>().enabled = true;
+  }
+
+  public void DisableCanvas() {
+    this.GetComponent<Canvas>().enabled = true;
+  }
+}
