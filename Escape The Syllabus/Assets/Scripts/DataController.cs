2115007654b5362bb52549b47a2459b044dfa982
Steven Yuh <stevenyuh96@gmail.com>
2019-02-18 12:21:13 -0500

    quiz screen added

--- Escape The Syllabus/Assets/Scripts/DataController.cs
+++ Escape The Syllabus/Assets/Scripts/DataController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class DataController : MonoBehaviour
 {
     public RoundData[] allRoundData;
--- Escape The Syllabus/Assets/_Complete-Game/Scripts/GameManager.cs
+++ Escape The Syllabus/Assets/_Complete-Game/Scripts/GameManager.cs
@@ -70,15 +70,22 @@ namespace Completed
         //This is called each time a scene is loaded.
         static private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
         {
-            instance.level++;
-						// updates level in database 
-						DatabaseUtil.instance.updateCurrentLevel(FirebaseChecks.instance.GetUserId(), instance.level);
-						if (instance.level > 1) {
-							GameObject.Find("Screens").SetActive(false);
-						}
-            instance.InitGame();
-        }
+            if (!arg0.Equals("Persistent"))
+            {
+                return;
+            }
+                instance.level++;
+                // updates level in database 
+                DatabaseUtil.instance.updateCurrentLevel(FirebaseChecks.instance.GetUserId(), instance.level);
+                if (instance.level > 1)
+                {
+                    GameObject.Find("Screens").SetActive(false);
+                }
 
+                instance.InitGame();
+            
+        }
+       
 
 		//Initializes the game for each level.
 		void InitGame()
